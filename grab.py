8e426e5505d17f901fa07627061acc47905e7594
Kar Epker <karepker+services@gmail.com>
2013-08-25 14:46:46 -0700

    Changed grab.py to contain only usage of other grabbers

--- grab.py
+++ grab.py
@@ -1,92 +1,51 @@
 #! /usr/bin/python3
 
 """
-Grabs the bing image of the day at given resolution
+Usage of wallpaper grabbers
 """
 
-__author__ = "Kar Epker"
+__author__ = "karepker@gmail.com (Kar Epker)"
+__copyright__ = "2013 Kar Epker (karepker@gmail.com)"
 
-import datetime
-import itertools
-import os
-import re
-import sys
-import urllib.parse
-import urllib.request
+import argparse
 
-class WallpaperGetter:
+from bing_grab import BingGrabber
+
+# dict of grabber name to grabber
+# specify grabbers in `grabbers` argument by their names given here
+GRABBERS = {
+    'bing': BingGrabber
+}
+
+def make_size_tuple(to_convert):
     """
-    A partially abstract class specifying methods for getting wallpapers
-    """
-    def __init__(self, image_ext, name):
-        self.image_ext = image_ext
-        self.name = name 
-
-    def get_page(self):
-        raise NotImplementedError
-
-    def save_images(self, directory):
-        """
-        Saves images retrieved from page in specified directory
-
-        Args:
-            directory (string): The directory in which to save the images
-        """
-        for image_url in self.get_page():
-            # determine an image name and get the image
-            today_string = datetime.datetime.today().strftime('%Y-%m-%d')
-            for i in itertools.count(start=0):
-                image_name = '%s %d%s' % (today_string, i, self.IMAGE_EXT)
-                if not os.path.exists(image_name):
-                    directory = os.path.join(directory, self.name)
-                    # make directory if it doesn't exist
-                    if (not os.path.exists(directory) or not
-                            os.path.isdir(directory)):
-                        os.mkdirs(directory)
-                    # save image
-                    full_path = os.path.join(directory, image_name)
-                    urllib.request.urlretrieve(image_url, full_path)
-                    break
-
-
-class BingWallpaperGetter(WallpaperGetter):
-    """
-    A class that gets the picture of the day from Bing
-    """
-
-    def __init__(self, **options):
-        self.REQUEST_PARAMS = {'format': 'xml', 'idx': 0, 'mkt': 'en-US', 
-            'n': 1}
-        self.REQUEST_BASE_URL = 'http://www.bing.com/HPImageArchive.aspx'
-        self.IMAGE_DOMAIN = 'http://www.bing.com'
-        self.image_size = options['size'] if 'size' in options else (1920, 
-            1080)
-        self.IMAGE_EXT = '.jpg'
-        self.NAME = 'bing'
-        super().__init__(self.IMAGE_EXT, self.NAME)
-
-    def get_page(self):
-        """
-        Gets the data from the page
-
-        Raises: urllib.error.URLError if URL doesn't exist
-        """
-        # make the request to get the page
-        request_params_string = urllib.parse.urlencode(self.REQUEST_PARAMS)
-        request_url = self.REQUEST_BASE_URL + '?' + request_params_string
-        response = urllib.request.urlopen(request_url).read().decode('utf-8')
-
-        # find the image urls with a regex
-        image_matches = re.findall(r'.*\<image\>.*\<urlBase\>(\S+)'
-            r'\<\/urlBase\>.*\<\/image\>.*', response, re.MULTILINE)
-        image_size_string = "_%sx%s" % (self.image_size)
-
-        # yield all images found
-        for image_base_url in image_matches:
-            yield (self.IMAGE_DOMAIN + image_base_url + image_size_string + 
-                self.IMAGE_EXT)
-
+    Makes a size tuple from the given string
     
+    Args:
+        to_convert (string): A string of the form "width,height" to convert
+
+    Returns:
+        A tuple of the form (width, height)
+    """
+    return tuple(int(i) for i in to_convert.split(',', 1))
+
+
 if __name__ == '__main__':
-    b = BingWallpaperGetter(size=(1920, 1080))
-    b.save_images('/media/windows/Users/Kar/Pictures/wallpapers/')
+    # set up arguments
+    parser = argparse.ArgumentParser(description='Download daily wallpapers')
+    parser.add_argument('grabbers', help='Which grabbers to run, choices: %s' 
+        % (', '.join(list(GRABBERS.keys()))), type=lambda x: x.split(','))
+    parser.add_argument('--size', '-s',  help='Size of images to grab '
+        '(when available)', type=make_size_tuple, default="1920,1080")
+    parser.add_argument('--base_dir', '-b', help='Where to save images',
+        type=str, default='/tmp')
+    args = parser.parse_args()
+
+    # create the grabbers
+    grabbers = []
+    for grabber in args.grabbers:
+        grabbers.append(GRABBERS[grabber](size=args.size))
+
+    # save images
+    for grabber in grabbers:
+        grabber.save_images(args.base_dir)
